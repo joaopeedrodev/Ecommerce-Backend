2de617a0148a0b8b373b6e0c370ab8c34d805376
João Pedro Tavares <joaopeedrotavares23@gmail.com>
2025-10-17 12:20:53 -0400

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,31 @@
+<?php
+class Database
+{
+    private $host = 'localhost';
+    private $db_name = 'ecommerce';
+    private $username = 'root';
+    private $password = '';
+    private $conn;
+
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
+                $this->username,
+                $this->password,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false
+                ]
+            );
+        } catch (PDOException $e) {
+            throw new Exception("Erro de conexão: " . $e->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
